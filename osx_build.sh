4cb23e222753da0b6d593809cc0076e607d78bd7
steve olsen <dev@steve.computer>
2020-04-21 13:20:06 -0500

    build right in?

--- osx_build.sh
+++ osx_build.sh
@@ -2,7 +2,8 @@
 
 mkdir -p build
 
-clang++ -Wall -O3 -std=c++2a -I3rdparty -o build/game game.cpp -L3rdparty/osx -lraylib -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL
-
 cp -r misc/Game.app build/
-mv build/game build/Game.app/Contents/MacOS/
+
+clang++ -Wall -O3 -std=c++2a -I3rdparty -o build/Game.app/Contents/MacOS/game game.cpp -L3rdparty/osx -lraylib -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL
+
+
