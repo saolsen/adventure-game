c9088d028a873397fc0781ac7a18aa62d8f8e693
steve olsen <dev@steve.computer>
2020-04-21 19:12:08 -0500

    try again

--- osx_build.sh
+++ osx_build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-mkdir -p build
+mkdir -p game-osx
 
-cp -r misc/Game.app build/
+cp -r misc/Game.app game-osx/
 
-clang++ -Wall -O3 -std=c++2a -I3rdparty -o build/Game.app/Contents/MacOS/game game.cpp -L3rdparty/osx -lraylib -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL
+clang++ -Wall -O3 -std=c++2a -I3rdparty -o game-osx/Game.app/Contents/MacOS/game game.cpp -L3rdparty/osx -lraylib -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL
 
-
+zip -r game-osx.zip game-osx
