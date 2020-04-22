0824c2ae4b2a729abcd8e6de09ef67cbeb956bc6
steve olsen <dev@steve.computer>
2020-04-21 19:43:28 -0500

    publish

---
+++ osx_publish.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Build it first.
+
+curl -L -o butler.zip https://broth.itch.ovh/butler/darwin-amd64/LATEST/archive/default
+unzip butler.zip
+chmod +x butler
+./butler -V
+./butler push game-osx.zip steveolsen/adventure-game:osx-alpha
